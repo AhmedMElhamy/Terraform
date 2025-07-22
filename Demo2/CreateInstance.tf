0a59db3176d5fb131d233669d42caec6ccbbd61c
AhmedMElhamy <147607671+AhmedMElhamy@users.noreply.github.com>
2025-07-22 16:13:58 +0300

    Update CreateInstance.tf

--- Demo2/CreateInstance.tf
+++ Demo2/CreateInstance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "MyFirstInstnace" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demoinstnce"
+    Name = "demoinstance"
   }
 
   security_groups = var.Security_Group
